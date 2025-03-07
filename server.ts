37c47228144a66fae1787ce2ab234345604add94
castrovc <vladimir_castrosv@outlook.com>
2025-03-07 12:32:53 -0600

    modified for use with products

--- server.ts
+++ server.ts
@@ -1,29 +1,37 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import multer from "multer";
 import csv from "csv-parser";
 import fs from "fs";
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(cors());
 
 const upload = multer({ dest: "uploads/" });
 
 interface CSVRow {
-  firstName: string;
-  secondName: string;
-  firstLastName: string;
-  secondLastName: string;
-  bankAccount: string;
-  nit: string;
-  dui: string;
-  branch: string;
-  isss: string;
-  charge: string;
+  name: string;
+  code: string;
+  description: string;
+  subCategoryId: number;
+  supplierId: number;
+  tipoItem: number;
+  tipoDeItem: string;
+  uniMedida: string;
+  unidaDeMedida: string;
+  stock: number;
+  minimumStock: number;
+  costoUnitario: number;
+  branchId: number;
+  price: number;
+  priceA: number;
+  priceB: number;
+  priceC: number;
 }
-app.post("/upload-csv", upload.single("file"), (req, res) => {
+
+app.post("/upload-csv", upload.single("file"), (req: Request, res: Response) => {
   const results: Array<CSVRow> = [];
 
   if (!req.file) {
@@ -34,27 +42,33 @@ app.post("/upload-csv", upload.single("file"), (req, res) => {
   let index = 0;
   fs.createReadStream(filePath)
     .pipe(csv())
-    .on("data", (row) => {
+    .on("data", (row: any) => {
       index++;
       results.push({
-        firstName : row.firstName,
-        secondName : row.secondName,
-        firstLastName : row.firstLastName,
-        secondLastName : row.secondLastName,
-        bankAccount : row.bankAccount,
-        nit : row.nit,
-        dui : row.dui,
-        branch : row.branch,
-        isss : row.isss,
-        charge : row.charge,
+        name: row.name,
+        code: index.toString(),
+        description: row.name,
+        subCategoryId: Number(row.subCategoryId),
+        supplierId: 1,
+        tipoItem: 1,
+        tipoDeItem: 'Bienes',
+        uniMedida: '59',
+        unidaDeMedida: 'Unidad',
+        stock: 0,
+        minimumStock: 0,
+        costoUnitario: 0,
+        branchId: 1,
+        price: row.price,
+        priceA: row.priceA,
+        priceB: row.priceB,
+        priceC: 0
       });
     })
     .on("end", () => {
       fs.unlinkSync(filePath);
-
       res.json(results);
     })
-    .on("error", (error) => {
+    .on("error", (error: Error) => {
       res.status(500).json({ error: error.message });
     });
 });
